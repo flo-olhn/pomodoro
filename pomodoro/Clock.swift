99b02cbda2d28320a4a3c8c724e5081929803584
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-26 16:51:15 +0200

    fix issues on clock + add buttons

--- pomodoro/Clock.swift
+++ pomodoro/Clock.swift
@@ -8,38 +8,55 @@
 import SwiftUI
 
 struct Clock: View {
+    
+    @State var w: CGFloat
+    @State var lw_h: CGFloat
+    @State var lw_m: CGFloat
+    @State var lw_s: CGFloat
     @State private var now = Date()
-    private let timer = Timer.publish(every: 0, on: .main, in: .common)
+    private let timer = Timer.publish(every: 1, on: .main, in: .common)
         .autoconnect()
     
     var body: some View {
         let cal = Calendar.current
-        let h = cal.component(.hour, from: now) < 10 ? "0\(cal.component(.hour, from: now))" : String(cal.component(.hour, from: now))
-        let m = cal.component(.minute, from: now) < 10 ? "0\(cal.component(.minute, from: now))" : String(cal.component(.minute, from: now))
-        let s = cal.component(.second, from: now) < 10 ? "0\(cal.component(.second, from: now))" : String(cal.component(.second, from: now))
         
-        let seconds = cal.component(.second, from: now)
-        let minutes = cal.component(.minute, from: now)
-        let hours = (cal.component(.hour, from: now) < 13) ? cal.component(.hour, from: now) : cal.component(.hour, from: now) - 12
+        let hour_string = cal.component(.hour, from: now) < 10 ? "0\(cal.component(.hour, from: now))" : String(cal.component(.hour, from: .now))
+        let minute_string = cal.component(.minute, from: now) < 10 ? "0\(cal.component(.minute, from: now))" : String(cal.component(.minute, from: .now))
+        let second_string = cal.component(.second, from: now) < 10 ? "0\(cal.component(.second, from: now))" : String(cal.component(.second, from: .now))
+        
+        let s = cal.component(.second, from: now)
+        let m = cal.component(.minute, from: now)
+        let h = cal.component(.hour, from: now) < 12 ? cal.component(.hour, from: now) : cal.component(.hour, from: now) - 12
+        
         ZStack {
-            Gauge(value: Double(hours) + Double(minutes) / 60.0, in: 0...12) {}
-                .gaugeStyle(ClockGaugeStyle(gradient: LinearGradient(colors: [.indigo, .blue], startPoint: .trailing, endPoint: .leading), lw: 8))
-                .animation(.linear(duration: 1), value: seconds)
-                .frame(width: 200)
-            Gauge(value: Double(minutes) + Double(seconds) / 60.0, in: 0...60) {}
-                .gaugeStyle(ClockGaugeStyle(gradient: LinearGradient(colors: [.purple, .blue], startPoint: .trailing, endPoint: .leading), lw: 6))
-                .animation(.linear(duration: 1), value: seconds)
-                .frame(width: 182)
-            Gauge(value: Double(seconds), in: 0...59) {}
-                .gaugeStyle(ClockGaugeStyle(gradient: LinearGradient(colors: [.teal, .blue], startPoint: .trailing, endPoint: .leading), lw: 4))
-                .animation(.linear(duration: 1), value: seconds)
-                .frame(width: 168)
-            Text("\(h) : \(m) : \(s)")
-                .font(.system(size: 20, weight: .heavy))
-                .foregroundStyle(Color(red: 0.4, green: 0.4, blue: 0.4, opacity: 0.4))
-                .onReceive(timer, perform: { time in
-                    now = time
-                })
+            Gauge(value: Double(h) + (Double(m)/60 + Double(s)/3600), in: 0...12) { }
+                .gaugeStyle(ClockGaugeStyle(gradient: LinearGradient(colors: [.indigo, .blue], startPoint: .trailing, endPoint: .leading), lw: lw_h))
+                .animation(.linear(duration: 1), value: s)
+                .frame(width: w - 100)
+            Gauge(value: Double(m) + Double(s) / 60.0, in: 0...60) { }
+                .gaugeStyle(ClockGaugeStyle(gradient: LinearGradient(colors: [.purple, .blue], startPoint: .trailing, endPoint: .leading), lw: lw_m))
+                .animation(.linear(duration: 1), value: s)
+                .frame(width: w - (100+lw_h+lw_m) - lw_m)
+            Gauge(value: Double(s), in: 0...60) { }
+                .gaugeStyle(ClockGaugeStyle(gradient: LinearGradient(colors: [.teal, .blue], startPoint: .trailing, endPoint: .leading), lw: lw_s))
+                .animation(.linear(duration: 1), value: s)
+                .frame(width: w - (100+lw_h+lw_m+lw_s) - (lw_h+lw_m+lw_s))
+            if (w > 150) {
+                Text("\(hour_string) : \(minute_string) : \(second_string)")
+                    .font(.system(size: w / 20, weight: .heavy))
+                    .foregroundStyle(Color(red: 0.4, green: 0.4, blue: 0.4, opacity: 0.4))
+                    .onReceive(timer, perform: { time in
+                        now = time
+                    })
+            } else {
+                Text("\(hour_string) : \(minute_string) : \(second_string)")
+                    .hidden()
+                    .font(.system(size: w / 20, weight: .heavy))
+                    .foregroundStyle(Color(red: 0.4, green: 0.4, blue: 0.4, opacity: 0.4))
+                    .onReceive(timer, perform: { time in
+                        now = time
+                    })
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
@@ -54,7 +71,7 @@ struct ClockGaugeStyle: GaugeStyle {
             Circle()
                 .foregroundColor(.clear)
             Circle()
-                .trim(from: 0, to: configuration.value)
+                .trim(from: 0.0, to: configuration.value)
                 .stroke(gradient, style: .init(lineWidth: lw, lineCap: .round))
                 .rotationEffect(.degrees(-90))
         }
@@ -62,5 +79,5 @@ struct ClockGaugeStyle: GaugeStyle {
 }
 
 #Preview {
-    Clock()
+    Clock(w: 300, lw_h: 8, lw_m: 6, lw_s: 4)
 }
--- pomodoro/ContentView.swift
+++ pomodoro/ContentView.swift
@@ -13,11 +13,35 @@ struct ContentView: View {
     @Query private var items: [Item]
     
     let gradient = Gradient(colors: [.pink, .blue])
+    @State var wdth: CGFloat = 0
+    
+    func start() {
+        wdth = 100
+    }
     
     var body: some View {
-        Clock()
+        VStack {
+            if (wdth == 100) {
+                Clock(w: 150, lw_h: 4, lw_m: 3, lw_s: 2)
+                    .frame(maxWidth: 50, maxHeight: 50)
+                    .position(x: 30, y: 30)
+            } else {
+                Clock(w: 300, lw_h: 8, lw_m: 6, lw_s: 4)
+                    .frame(maxWidth: 200, maxHeight: 200)
+            }
+            HStack {
+                Button("Start Session", action: start)
+                Button("\(Image(systemName: "gearshape.fill"))", action: start)
+                    .frame(width: 40, height: 40)
+                    .background(Color(red: 0, green: 0.5, blue: 0.5))
+                    .padding()
+            }
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
+
 
 #Preview {
     ContentView()
